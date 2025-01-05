d4aa0025e9c0cd52e3641d120f44b1f92f2e92c6
Farah-94 <farahshigri94@gmail.com>
2025-01-05 00:07:30 +0000

    index, css, js

---
+++ j.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  let boxTwo = document.getElementsByClassName("box2");
+  boxTwo.addEventListener("mouseover",function(e){
+
+
+
+  });
+
+
