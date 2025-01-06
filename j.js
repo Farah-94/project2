6b8e24bcb5f7a659b26ad1becd209502b30276c9
Farah-94 <farahshigri94@gmail.com>
2025-01-06 18:51:41 +0000

    index page css

--- j.js
+++ j.js
@@ -1,17 +1,30 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  let box1 = document.getElementsByClassName("box1");
-  box1.addEventListener("mouseenter",function(e){
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
-    console.log(box1.length);
-box1.style.backgroundcolor="#090909";
-  });
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
